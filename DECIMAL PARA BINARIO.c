94d4020c1fd06b65d018f21c355f4bf4f74b9343
José_DaniloS <154340363+JoseDaniloS@users.noreply.github.com>
2024-03-25 21:32:54 -0300

    Add files via upload

---
+++ DECIMAL PARA BINARIO.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <locale.h>
+
+//PROTOTIPO DA FUNÇÃO
+void dBinario(int numero);
+
+//FUNÇÃO PRINCIPAL
+int main(){
+	
+	//DEFINIÇÃO DE VARIAVEIS
+	int numero;
+	
+	//MUDANDO O IDIOMA PARA PORTUGUES
+	setlocale(LC_ALL,"Portuguese");
+	
+	//DISPLAY
+	printf("=====================================\n");
+	printf("==CONVERSÃO-DE-DECIMAL-PARA-BINARIO==\n\n");
+	
+	//SOLICITANDO AO USUARIO O VALOR QUE ELE DESEJA CONVERTER
+	printf("INSIRA O VALOR QUE DESEJA CONVERTER: ");
+	scanf("%i", &numero);
+	
+	//CHAMANDO A FUNÇÃO (qBinario)
+	dBinario(numero);
+	
+	
+	return 0;
+}
+
+//FUNÇÃO QUE CONVERTE O NUMERO DIGITADO PELO USUARIO PARA BINARIO
+void dBinario(int numero){
+	
+	//DEFINIÇÃO DE VARIAVEIS E VETOR
+	int binario[32];
+	int i = 0;
+	int j;
+	int n2;
+	
+	//N2 RECEBE O MESMO VALOR QUE A VARIAVEL numero
+	n2 = numero;
+	
+	//LOOPING VERIFICAR SE O A DIVISAO DO NUMERO RESTA 0 OU 1 E ARMAZENA EM UM VETOR
+	while(numero > 0){
+		binario[i] = numero % 2;
+		numero = numero / 2;
+		i++;
+	}
+	
+	//LOOPING QUE INVERTE O VALOR DOS VETORES
+	 printf("%i EM BINARIO É: ", n2);
+    for(j = i - 1; j >= 0; j--){
+        printf("%d", binario[j]);
+    }
+
+}
